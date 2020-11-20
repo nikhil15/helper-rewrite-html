30710026789d35a552339106217ad45046097c52
nikhil15 <nikhilgudadhe63@gmail.com>
2020-11-20 18:28:25 +0530

    Update helper-rewite-html.php

--- helper-rewite-html.php
+++ helper-rewite-html.php
@@ -1,3 +1,69 @@
+/*************** YouTube Video Block ***************/
+
+function rocket_helper_lazyload_exclude_pattern( array $pattern ) {
+    
+    $pattern[] = 'youtube';    
+    return $pattern;
+}
+
+add_filter( 'rocket_lazyload_iframe_excluded_patterns', 'rocket_helper_lazyload_exclude_pattern' );
+
+---------------------------------------------------------
+
+foreach ($newHtml->find('iframe[src*="https://www.youtube.com/embed/MU0Yp0qmYEs?rel=0"]') as $iframe) {
+	$iframe->setAttribute("data-type", "wprocket-youtube-video");
+	$iframe->setAttribute("data-src", $iframe->src);
+	$iframe->removeAttribute("src");
+}
+
+---------------------------------------------------------
+
+var helperYoutubeVideoTimer=setTimeout(helperYoutubeVideo, helper_youtube_delay);
+helperUserInteractionEvents.forEach(function(event){window.addEventListener(event,helperYoutubeVideoEvent,{passive:!0})});
+
+function helperYoutubeVideoEvent(){
+if(helper_youtube_video){helperYoutubeVideo();}
+clearTimeout(helperYoutubeVideoTimer);
+helperUserInteractionEvents.forEach(function(event){window.removeEventListener(event,helperYoutubeVideoEvent,{passive:!0})});
+}
+
+function helperYoutubeVideo(){
+document.querySelectorAll("iframe[data-type='wprocket-youtube-video']").forEach(
+function(elem){elem.setAttribute("src",elem.getAttribute("data-src"));
+elem.removeAttribute("data-src")})
+helper_youtube_video=false;
+}
+
+/*************** YouTube Video Block ***************/
+
+/*************** YouTube Vimeo Block ***************/
+
+foreach ($newHtml->find('iframe[src*="https://player.vimeo.com/video/416158069?autoplay=1&title=0&byline=0&portrait=0"]') as $iframe) {
+	$iframe->setAttribute("data-type", "wprocket-vimeo-video");
+	$iframe->setAttribute("data-src", $iframe->src);
+	$iframe->removeAttribute("src");
+}
+
+---------------------------------------------------------
+
+var helperVimeoVideoTimer=setTimeout(helperVimeoVideo, helper_vimeo_delay);
+helperUserInteractionEvents.forEach(function(event){window.addEventListener(event,helperVimeoVideoEvent,{passive:!0})});
+
+function helperVimeoVideoEvent(){
+if(helper_vimeo_video){helperVimeoVideo();}
+clearTimeout(helperVimeoVideoTimer);
+helperUserInteractionEvents.forEach(function(event){window.removeEventListener(event,helperVimeoVideoEvent,{passive:!0})});
+}
+
+function helperVimeoVideo(){
+document.querySelectorAll("iframe[data-type='wprocket-vimeo-video']").forEach(
+function(elem){elem.setAttribute("src",elem.getAttribute("data-src"));
+elem.removeAttribute("data-src")})
+helper_vimeo_video=false;
+}
+
+/*************** YouTube Vimeo Block ***************/
+
 /*************** External CSS Block ***************/
 
 if(preg_match("/cdn.jsdelivr.net\/fontawesome\/4.7.0\/css\/font-awesome.min/i", $stylesheet->href)) {
@@ -32,3 +98,4 @@ helper_external_css=false;
 }
 
 /*************** External CSS Block ***************/
+
